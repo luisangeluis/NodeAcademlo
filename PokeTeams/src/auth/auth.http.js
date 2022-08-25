d69cadbc0108fd1247415d0dd924b53ff0863930
luisangeluis <luisangeluis@gmail.com>
2022-08-24 19:39:18 -0500

    Hacer pruebas de las rutas clase 19 de agosto terminado.

--- PokeTeams/src/auth/auth.http.js
+++ PokeTeams/src/auth/auth.http.js
@@ -1,24 +1,24 @@
-const jwt =require('jsonwebtoken');
-const authController =require('./auth.controllers')
+const jwt = require('jsonwebtoken');
+const authController = require('./auth.controllers')
 
-const loginUser =(req,res)=>{
-  const data =req.body;
+const loginUser = (req, res) => {
+  const data = req.body;
 
-  if(!data.email || !data.password){
-    return res.status(400).json({message:'Missing data'})
+  if (!data.email || !data.password) {
+    return res.status(400).json({ message: 'Missing data' })
   }
 
-  const response =authController.login(data.email,data.password);
+  const response = authController.login(data.email, data.password);
 
-  if(response){
-    const token =jwt.sign({
+  if (response) {
+    const token = jwt.sign({
       id: response.id,
-      email:response.email
-    },'academlo')
-    return res.status(200).json({message:'Tus credenciales son exitosas',token})
-  }else{
-    return res.status(401).json({message:'Invalid credentials'});
+      email: response.email
+    }, 'academlo')
+    return res.status(200).json({ message: 'Tus credenciales son exitosas', token })
+  } else {
+    return res.status(401).json({ message: 'Invalid credentials' });
   }
 }
 
-exports.loginUser =loginUser;
+exports.loginUser = loginUser;
